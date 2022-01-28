998919b8edd929d81ba68f6b4311e361c56f5b09
Sally <proto1998@gmail.com>
2022-01-28 19:28:28 +0100

    Ajout image profile/ Auth

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require("path");
+const bodyParser = require('body-parser');
 const app = express();
 
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 // Evite les bloquage CORS en autorisant les entêtes :
 app.use((req, res, next) => {
@@ -13,6 +16,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/post', postRoutes);
 app.use('/api/auth', userRoutes);
